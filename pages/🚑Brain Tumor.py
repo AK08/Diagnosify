3a1d8e85d365dc6e61c6bd20172c322e9be5e4c0
AK08 <alenkottaram2000@gmail.com>
2023-08-08 15:20:30 +0530

    Initial commit

---
+++ pages/🚑Brain Tumor.py
@@ -0,0 +1,48 @@
+import os
+import streamlit as st
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+from keras.models import load_model
+from werkzeug.utils import secure_filename
+
+# Get the absolute path to the model file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_file_path = os.path.join(current_dir, 'BrainTumor10EpochsCategorical.h5')
+
+# Define the function to get class names
+def get_className(classNo):
+    if classNo == 0:
+        return "No Brain Tumor"
+    elif classNo == 1:
+        return "Yes Brain Tumor"
+
+# Define the function to get the result from the model
+def getResult(img):
+    image = cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((64, 64))
+    image = np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result = model.predict(input_img)
+    class_index = np.argmax(result)
+    return class_index
+
+# Main function to run the web application
+def main():
+    st.title("Brain Tumor Image Classification")
+    st.text("Upload an MRI scan image to check for brain tumor")
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+    if uploaded_file is not None:
+        file_path = os.path.join('uploads', secure_filename(uploaded_file.name))
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        value = getResult(file_path)
+        result = get_className(value)
+        st.text(f"Prediction: {result}")
+
+if __name__ == "__main__":
+    main()
---
+++ 🏠Home.py
@@ -0,0 +1,64 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Brain Disease Image Classification",
+    page_icon="🧠",
+)
+
+st.write("# Welcome to Brain Disease Image Classification! 👋")
+
+st.markdown(
+    """
+    Our project aims to utilize the power of Deep Learning and Artificial Intelligence to help diagnose various brain diseases from MRI scan images. We understand the critical importance of early detection and accurate diagnosis in treating brain-related conditions, and that's why we have developed this intelligent system.
+    """
+)
+
+st.markdown("### How it Works:")
+st.markdown(
+    """
+- Upload an MRI scan image of the brain.
+- Our advanced model will analyze the image using cutting-edge techniques.
+- The system will provide a comprehensive diagnosis, including whether the person has a brain tumor and other brain diseases.
+"""
+)
+
+st.markdown("### Why Choose Us:")
+st.markdown(
+    """
+- Highly Accurate: Our model has been trained on a vast dataset of brain MRI scans to ensure accurate predictions.
+- Fast and Efficient: The prediction process is quick, providing you with results in just a few seconds.
+- User-Friendly Interface: Our intuitive interface makes it easy for anyone to use, even without prior technical knowledge.
+"""
+)
+
+st.markdown("### Benefits of Early Detection:")
+st.markdown(
+    """
+- Early detection of brain tumors and diseases can significantly improve treatment outcomes.
+- Timely diagnosis enables healthcare professionals to plan the best course of action for patients.
+- Our system aids medical practitioners in making well-informed decisions, leading to better patient care.
+"""
+)
+
+st.markdown("### Data Privacy and Security:")
+st.markdown(
+    """
+We understand the sensitivity of medical data and assure you that your uploaded images are handled with the utmost care and privacy. Your data is encrypted and not shared with any third parties.
+"""
+)
+
+st.markdown("### Limitations:")
+st.markdown(
+    """
+While our system provides valuable insights, it is essential to remember that it is not a substitute for professional medical advice. Always consult a qualified healthcare professional for a thorough diagnosis and treatment plan.
+"""
+)
+
+st.markdown("### Get Started:")
+st.markdown(
+    """
+Try out our Brain Disease Image Classification Project now! Upload an MRI scan image and receive instant results. We are committed to making a positive impact on healthcare and contributing to a healthier world.
+
+Thank you for choosing our platform. Together, let's improve brain disease diagnosis and patient care!
+"""
+)
