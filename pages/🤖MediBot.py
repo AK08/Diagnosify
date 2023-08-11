ea46c30bcbfcc8ed928fe96ff2ae580f5eae7532
AK08 <alenkottaram2000@gmail.com>
2023-08-11 23:44:33 +0530

    c

--- pages/🤖MediBot.py
+++ pages/🤖MediBot.py
@@ -15,7 +15,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Hi! How can I help you?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
